18b44f2c8a0db7c05989c750ed3e52955c237f02
Matt Cheung <matt.wh.c@gmail.com>
2024-07-08 11:46:50 +0100

    add fenwick tree

---
+++ fenwick_tree/one_dimensional_fenwick_tree/c_sharp/OneDimensionalFenwickTree.Tests/FenwickTreeTests.cs
@@ -0,0 +1,30 @@
+namespace OneDimensionalFenwickTree.Tests;
+
+public class FenwickTreeTests
+{
+    [Theory]
+    [InlineData("0,0,0,0,0,0,0,0", "5,5,0,5,0,0,0,5", 0, 5)]
+    [InlineData("5,5,0,5,0,0,0,5", "5,5,2,7,0,0,0,7", 2, 2)]
+    [InlineData("5,5,2,7,0,0,0,7", "5,5,2,7,4,4,0,11", 4, 4)]
+    public void Update_Should_Update_Index_Value_And_Parents(string original, string expected, int i, int v)
+    {
+        var tree = original.Split(',').Select(x => int.Parse(x)).ToArray();
+        var expectedTree = expected.Split(',').Select(x => int.Parse(x)).ToArray();
+        FenwickTree.Update(tree, i, v);
+
+        Assert.Equal(expectedTree, tree);
+    }
+
+    [Theory]
+    [InlineData("5,5,2,7,4,4,0,11", 7, 11)]
+    [InlineData("5,5,2,7,4,4,0,11", 4, 11)]
+    [InlineData("5,5,2,7,4,4,0,11", 3, 7)]
+    public void Get_Should_Return_Sum_From_Zero_To_Index(string treeValues, int i, int expected)
+    {
+          var tree = treeValues.Split(',').Select(x => int.Parse(x)).ToArray();
+
+         var sum= FenwickTree.Get(tree, i);
+           Assert.Equal(expected, sum);
+
+    }
+}
---
+++ fenwick_tree/one_dimensional_fenwick_tree/c_sharp/OneDimensionalFenwickTree/FenwickTree.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace OneDimensionalFenwickTree;
+public static class FenwickTree
+{
+    public static int Get(int[] tree, int index)
+    {
+        var sum = 0;
+        for (int i = index + 1; i > 0; i -= i & -i)
+        {
+            sum += tree[i - 1];
+        }
+
+        return sum;
+    }
+
+    public static void Update(int[] tree, int index, int value)
+    {
+        for (int i = index + 1; i <= tree.Length; i += (i & -i))
+        {
+            tree[i - 1] += value;
+        }
+    }
+
+}
